bab00d9daf08126ff44b57c0efae4353030b8f42
Cong Gao @ CEE-MacBooK16 <cnggao@gmail.com>
2025-07-04 11:34:37 -0400

    update della

--- terminal-matrix_della.sh
+++ terminal-matrix_della.sh
@@ -11,50 +11,18 @@ if ! command -v konsole &> /dev/null; then
     exit 1
 fi
 
-# Get screen dimensions
-if command -v xrandr &> /dev/null; then
-    # More reliable than xdpyinfo
-    SCREEN=$(xrandr | grep primary | grep -o '[0-9]\+x[0-9]\+')
-    SCREEN_WIDTH=${SCREEN%x*}
-    SCREEN_HEIGHT=${SCREEN#*x}
-else
-    SCREEN_WIDTH=1920
-    SCREEN_HEIGHT=1080
-fi
+TERM_COLS=$((1726 / COLS))
+TERM_ROWS=$((992 / ROWS))
 
-echo "Screen: ${SCREEN_WIDTH}x${SCREEN_HEIGHT}"
-echo "Creating ${ROWS}x${COLS} konsole grid..."
-
-#############################################
-# WINDOW SIZE CONFIGURATION
-#############################################
-# 
-# Konsole geometry uses: WIDTHxHEIGHT+X+Y
-# WIDTH/HEIGHT are in characters (not pixels!)
-# X/Y are pixel positions
-#
-# To make windows BIGGER, increase these:
-TERM_COLS=120    # Characters wide (try: 80, 100, 120, 150)
-TERM_ROWS=40     # Characters tall (try: 24, 35, 40, 50)
-
-# Screen layout (pixels)
-MARGIN=10        # Edge margin
-TASKBAR=40       # Bottom panel height
-#############################################
-
-# Calculate positions
-USABLE_W=$((SCREEN_WIDTH - 2*MARGIN))
-USABLE_H=$((SCREEN_HEIGHT - TASKBAR - 2*MARGIN))
-
-STEP_X=$((USABLE_W / COLS))
-STEP_Y=$((USABLE_H / ROWS))
+STEP_X=$((1726 / COLS))
+STEP_Y=$((992 / ROWS))
 
 # Launch windows
 N=1
 for ((r=0; r<ROWS; r++)); do
     for ((c=0; c<COLS; c++)); do
-        X=$((MARGIN + c * STEP_X))
-        Y=$((MARGIN + r * STEP_Y))
+        X=$(( c * STEP_X))
+        Y=$(( r * STEP_Y))
         
         konsole \
             --separate \
